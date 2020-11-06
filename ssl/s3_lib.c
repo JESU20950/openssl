113a95cebc78509010a03c97db826d9028c1a823
root <jmr_sultan@hotmail.com>
2020-11-06 04:31:23 +0100

    time

--- ssl/s3_lib.c
+++ ssl/s3_lib.c
@@ -4829,7 +4829,7 @@ int ssl_derive(SSL *s, EVP_PKEY *privkey, EVP_PKEY *pubkey, int gensecret)
     }
 	struct timespec ts_end;
     timespec_get(&ts_end, TIME_UTC);
-	printf("%091d,", ts_end.tv_nsec-ts_begin.tv_nsec);
+	printf("%ld,", ts_end.tv_nsec-ts_begin.tv_nsec);
     if (gensecret) {
         /* SSLfatal() called as appropriate in the below functions */
         if (SSL_IS_TLS13(s)) {
